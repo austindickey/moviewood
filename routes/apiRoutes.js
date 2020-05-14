c3a0b15bc882165cd5e278c19c5fce7864f8f267
austindickey <austindickey@me.com>
2020-05-14 00:34:28 -0500

    cleaning up the apiRoutes file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,18 +4,14 @@ require("dotenv").config()
 // const controller = require("../controllers/userController")
 
 function movieSearch(req, res) {
-    // const apiKey = process.env.apiKey
     const tmdbApiKey = process.env.tmdbApiKey
     let searchQuery = req.params.search
-
-    console.log("TMDB API KEY: ", tmdbApiKey)
     
     const url = `https://tastedive.com/api/similar?type=movies&q=${searchQuery}`
 
     axios.get(url)
         .then(response => {
             let recommendations = response.data.Similar.Results
-            // res.send(recommendations)
 
             console.log("Recommendations: ", recommendations)
 
@@ -29,27 +25,25 @@ function movieSearch(req, res) {
                         
                         for (let x = 0; x < single.length; x++) {
 
-                            if (x === single.length -1)
-
-                            if (single[x] === undefined) {
-                                continue
-                            } else if (single[x].title.toLowerCase() === recommendations[i].Name.toLowerCase()) {
+                            if (single[x].title.toLowerCase() === recommendations[i].Name.toLowerCase()) {
                                 console.log("Single[i]: ", single[x])
                                 movieData.push(single[x])
                                 break
                             }
+
                         }
+                        
                     })
                     .catch(err => {
                         console.log(err)
                     })
+
             }
 
-            console.log("Movie Data: ", movieData)
+            if (movieData.length === recommendations.length) {
+                res.send(movieData) // I still have to find someway to tell it to wait to send the data
+            }
 
-            res.send(movieData)
-
-            
         })
         .then()
         .catch(err => {
@@ -59,7 +53,6 @@ function movieSearch(req, res) {
 }
 
 function showSearch(req, res) {
-    // const apiKey = process.env.apiKey
     let searchQuery = req.params.search
     
     const url = `https://tastedive.com/api/similar?type=shows&q=${searchQuery}`
